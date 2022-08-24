d15f2101a33ae6d13fc9f5bd0f56859553165630
kazuhiro ogawa <tosdsmpamandk@gmail.com>
2022-08-24 13:40:51 +0900

    Merge branch 'master' of https://github.com/kaz-ogawa/arduinoGitTest
    # Conflicts:
    #	arduinoGitTest.ino

--- arduinoGitTest.ino
+++ arduinoGitTest.ino
@@ -1,8 +1,7 @@
 #define LED_BTN 7
-#define SERVO_BTN 7
+#define SERVO_BTN 8
 #define REDLED_BTN 1
-#define GREENLED_BTN 4
-
+#define BLUELED_BTN 2
 
 void setup() {
   // put your setup code here, to run once:
